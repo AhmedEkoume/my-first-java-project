26823ceb88156363183676cc48cacb8a16689cda
Ahmed Ekoume <ahmed.ekoume@meltwater.org>
2022-10-16 20:03:09 +0000

    My second commit

--- TryingMethods.java
+++ TryingMethods.java
@@ -1,10 +1,12 @@
+//This class has 4 methods , two static and two dynamic
 public class TryingMethods {
     public static void main(String[] args) {
 
         myFirstStatic();
+        mySecondStatic();
 
     }
-    public static void myFirstStatic() {
+    public static void myFirstStatic(){
             
             int number = 1;
             int result = 0;
@@ -12,14 +14,29 @@ public class TryingMethods {
             
             while (number < 1000){
 
-                System.out.println(result); 
+                
                 result += number;
                 number = number+1;
                
                
             }
+            System.out.println(result); 
+        }
+
+    public static void mySecondStatic(){
+        int result2 = 0;
+
+        for(int i = 12; i<103; i++ ){
+
+            if(i%2 ==0){
+                
+                result2 = result2 + i;
+            }
             
         }
+
+        System.out.println(result2);
+    }
 }
         
 
